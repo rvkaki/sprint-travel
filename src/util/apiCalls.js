fcc537d5635de2e4428cfef4c735776868e20e1b
Ricardo Vieira <rjvmcv.98@gmail.com>
2021-02-05 15:39:25 +0000

    offers and offerGroups endpoints

---
+++ src/util/apiCalls.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const getOfferGroups = async () => {
+  try {
+    const res = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/offer-groups`
+    );
+    if (res.statusText === 'OK') return res.data;
+    return [];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getOffers = async () => {
+  try {
+    const res = await axios.get(`${process.env.REACT_APP_SERVER_URL}/offers`);
+    if (res.statusText === 'OK') return res.data;
+    return [];
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,17 @@
 import { Box, Flex } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
+import { getOfferGroups, getOffers } from '../util/apiCalls';
 
 const Home = () => {
+  const [offerGroups, setOfferGroups] = useState();
+  const [offers, setOffers] = useState();
+
+  useEffect(() => {
+    getOfferGroups().then(data => setOfferGroups(data));
+    getOffers().then(data => setOffers(data));
+  }, []);
   return (
     <Box>
       <Header />
